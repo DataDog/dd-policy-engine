49dd88771e19348420e8373a221f13a720b98146
Damien Mehala <damien.mehala@datadoghq.com>
2025-09-19 17:39:48 +0200

    fetch flatcc from git commit

--- cmake/Findflatcc.cmake
+++ cmake/Findflatcc.cmake
@@ -1,54 +1,49 @@
-# Copyright 2014 Stefan.Eilemann@epfl.ch
-# Copyright 2014 Google Inc. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# Find the flatcc schema compiler
-#
-# Output Variables:
-# * FLATCC_FOUND
-# * FLATCC_INCLUDE_DIR
-# * FLATCC_RUNTIME_LIBRARY
+include(FetchContent)
 
-set(FLATCC_CMAKE_DIR ${CMAKE_CURRENT_LIST_DIR})
+set(FLATCC_TEST OFF CACHE BOOL "" FORCE)
+set(FLATCC_CXX_TEST OFF CACHE BOOL "" FORCE)
+set(FLATCC_DEBUG_CLANG_SANITIZE OFF CACHE BOOL "" FORCE)
+set(FLATCC_PORTABLE OFF CACHE BOOL "" FORCE)
+set(FLATCC_ALLOW_WERROR OFF CACHE BOOL "" FORCE)
+set(FLATCC_RTONLY ON CACHE BOOL "" FORCE)
+set(FLATCC_INSTALL ON CACHE BOOL "" FORCE)
 
-find_path(FLATCC_INCLUDE_DIR
-  NAMES flatcc/flatcc.h
-  HINTS
-  PATH_SUFFIXES include
-  PATHS "${VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}"
+set(CMAKE_POLICY_VERSION_MINIMUM "3.5")
+cmake_policy(SET CMP0175 OLD)
+
+# Temporarily suppres INFO messages from `flatc`. Keep a backup of our C flags because
+# `flatcc` updates global C flags.
+set(_OLD_C_FLAGS ${CMAKE_C_FLAGS})
+set(_OLD_LOG_LEVEL ${CMAKE_MESSAGE_LOG_LEVEL})
+set(CMAKE_MESSAGE_LOG_LEVEL WARNING)
+
+FetchContent_Declare(
+  flatcc
+  GIT_REPOSITORY "https://github.com/dvidelabs/flatcc.git"
+  GIT_TAG 47af7e601f511e80bcb85f28adf06af27c6a6b00
+  FIND_PACKAGE_ARGS NAMES flatcc
+  SYSTEM
+  EXCLUDE_FROM_ALL
 )
 
-find_library(FLATCC_RUNTIME_LIBRARY
-  NAMES flatccrt
-  PATHS "${VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}"
-)
+FetchContent_MakeAvailable(flatcc)
 
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(flatcc
-  REQUIRED_VARS FLATCC_INCLUDE_DIR FLATCC_RUNTIME_LIBRARY
-)
+# Reset C flags and log level.
+set(CMAKE_C_FLAGS ${_OLD_C_FLAGS})
+set(CMAKE_MESSAGE_LOG_LEVEL ${_OLD_LOG_LEVEL})
 
-if(FLATCC_FOUND)
-  add_library(flatcc-crt STATIC IMPORTED)
-  add_library(flatcc::crt ALIAS flatcc-crt)
-
-  set_target_properties(flatcc-crt PROPERTIES
-    IMPORTED_LOCATION ${FLATCC_RUNTIME_LIBRARY}
-    INTERFACE_INCLUDE_DIRECTORIES "${FLATCC_INCLUDE_DIR}"
-  )
+if(NOT TARGET flatccrt)
+  message(FATAL_ERROR "flatcrt target was not imported")
 endif()
 
-if(NOT TARGET flatcc::crt)
-  message(FATAL_ERROR "flatcc::crt target was not imported")
-endif()
+# Fixing flatccrt target
+get_target_property(FLATCC_INCLUDE_DIRS flatccrt INTERFACE_INCLUDE_DIRECTORIES)
+set_target_properties(flatccrt PROPERTIES 
+    INTERFACE_INCLUDE_DIRECTORIES ""
+)
+target_include_directories(flatccrt INTERFACE 
+    $<BUILD_INTERFACE:${flatcc_SOURCE_DIR}/include>
+    $<INSTALL_INTERFACE:include>
+)
+
+add_library(flatcc::crt ALIAS flatccrt)
