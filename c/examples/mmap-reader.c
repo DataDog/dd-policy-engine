866bfaba9ec96538eddf03106b1d59ae42c1b548
Damien Mehala <damien.mehala@datadoghq.com>
2025-09-22 15:51:44 +0200

    chore: prepare open sourcing [1/x] (#6)

--- c/examples/mmap-reader.c
+++ c/examples/mmap-reader.c
@@ -1,4 +1,10 @@
-
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed
+ * under the Apache 2.0 License. This product includes software developed at
+ * Datadog (https://www.datadoghq.com/).
+ *
+ * Copyright 2025-Present Datadog, Inc.
+ */
 #include <dd/policies/action.h>
 #include <dd/policies/error_codes.h>
 #include <dd/policies/eval_ctx.h>
