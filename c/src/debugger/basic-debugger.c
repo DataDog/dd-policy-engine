069c257ee2b56360b5bd193d0171082edcbd56d5
Octavio Roscioli <octavio.roscioli@datadoghq.com>
2025-11-20 10:59:11 -0500

    rm comments

--- c/src/debugger/basic-debugger.c
+++ c/src/debugger/basic-debugger.c
@@ -168,7 +168,6 @@ void print_policies(const uint8_t *buffer, size_t size) {
       continue;
     }
 
-    // 🔎 NEW: show policy description + version
     const char *p_desc = dd_ns(Policy_description)(policy);
     int64_t version = dd_ns(Policy_version)(policy);
     printf("Policy[%zu]: desc='%s' version=%" PRId64 "\n", ix, p_desc ? p_desc : "", version);
@@ -176,7 +175,6 @@ void print_policies(const uint8_t *buffer, size_t size) {
     dd_ns(NodeTypeWrapper_table_t) rules = dd_ns(Policy_rules)(policy);
     printTree(rules, "", true);
 
-    // 🔎 NEW: show actions for this policy
     print_actions(policy);
 
     printf("---------------[%zu]---------------\n\n", ix);
