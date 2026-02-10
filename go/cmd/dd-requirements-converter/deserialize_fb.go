8c72742101301fb28cbfc5501667cd0aa83b35cd
Anna Cai <anna.cai@datadoghq.com>
2026-02-10 10:37:41 -0500

    write deserialized output to file

--- go/cmd/dd-requirements-converter/deserialize_fb.go
+++ go/cmd/dd-requirements-converter/deserialize_fb.go
@@ -4,6 +4,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"strings"
 
@@ -11,9 +12,11 @@ import (
 	"github.com/DataDog/dd-policy-engine/go/schema/dd/wls"
 )
 
+var output io.Writer = os.Stdout
+
 func printNode(wrapper *wls.NodeTypeWrapper, indent string) {
 	nodeType := wrapper.NodeType()
-	fmt.Printf("%sNodeType: %s\n", indent, nodeType.String())
+	fmt.Fprintf(output, "%sNodeType: %s\n", indent, nodeType.String())
 
 	switch nodeType {
 	case wls.NodeTypeEvaluatorNode:
@@ -22,8 +25,8 @@ func printNode(wrapper *wls.NodeTypeWrapper, indent string) {
 			var evalNode wls.EvaluatorNode
 			evalNode.Init(table.Bytes, table.Pos)
 			
-			fmt.Printf("%s  Description: %s\n", indent, string(evalNode.Description()))
-			fmt.Printf("%s  EvalType: %s\n", indent, evalNode.EvalType().String())
+			fmt.Fprintf(output, "%s  Description: %s\n", indent, string(evalNode.Description()))
+			fmt.Fprintf(output, "%s  EvalType: %s\n", indent, evalNode.EvalType().String())
 			
 			// If it's a string evaluator, print details
 			if evalNode.EvalType() == wls.EvaluatorTypeStrEvaluator {
@@ -31,18 +34,18 @@ func printNode(wrapper *wls.NodeTypeWrapper, indent string) {
 				if evalNode.Eval(&evalTable) {
 					var strEval wls.StrEvaluator
 					strEval.Init(evalTable.Bytes, evalTable.Pos)
-					fmt.Printf("%s  Field: %s\n", indent, strEval.Id().String())
-					fmt.Printf("%s  Compare: %s\n", indent, strEval.Cmp().String())
-					fmt.Printf("%s  Value: %s\n", indent, string(strEval.Value()))
+					fmt.Fprintf(output, "%s  Field: %s\n", indent, strEval.Id().String())
+					fmt.Fprintf(output, "%s  Compare: %s\n", indent, strEval.Cmp().String())
+					fmt.Fprintf(output, "%s  Value: %s\n", indent, string(strEval.Value()))
 				}
 			} else if evalNode.EvalType() == wls.EvaluatorTypeNumEvaluator {
 				var evalTable flatbuffers.Table
 				if evalNode.Eval(&evalTable) {
 					var numEval wls.NumEvaluator
 					numEval.Init(evalTable.Bytes, evalTable.Pos)
-					fmt.Printf("%s  Field: %s\n", indent, numEval.Id().String())
-					fmt.Printf("%s  Compare: %s\n", indent, numEval.Cmp().String())
-					fmt.Printf("%s  Value: %d\n", indent, numEval.Value())
+					fmt.Fprintf(output, "%s  Field: %s\n", indent, numEval.Id().String())
+					fmt.Fprintf(output, "%s  Compare: %s\n", indent, numEval.Cmp().String())
+					fmt.Fprintf(output, "%s  Value: %d\n", indent, numEval.Value())
 				}
 			}
 		}
@@ -53,14 +56,14 @@ func printNode(wrapper *wls.NodeTypeWrapper, indent string) {
 			var composite wls.CompositeNode
 			composite.Init(table.Bytes, table.Pos)
 			
-			fmt.Printf("%s  Description: %s\n", indent, string(composite.Description()))
-			fmt.Printf("%s  Operation: %s\n", indent, composite.Op().String())
-			fmt.Printf("%s  Children (%d):\n", indent, composite.ChildrenLength())
+			fmt.Fprintf(output, "%s  Description: %s\n", indent, string(composite.Description()))
+			fmt.Fprintf(output, "%s  Operation: %s\n", indent, composite.Op().String())
+			fmt.Fprintf(output, "%s  Children (%d):\n", indent, composite.ChildrenLength())
 			
 			for i := 0; i < composite.ChildrenLength(); i++ {
 				var child wls.NodeTypeWrapper
 				if composite.Children(&child, i) {
-					fmt.Printf("%s  [%d]:\n", indent, i)
+					fmt.Fprintf(output, "%s  [%d]:\n", indent, i)
 					printNode(&child, indent+"    ")
 				}
 			}
@@ -70,44 +73,65 @@ func printNode(wrapper *wls.NodeTypeWrapper, indent string) {
 
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Println("Usage: go run deserialize_fb.go <file.bin>")
+		fmt.Println("Usage: go run deserialize_fb.go <file.bin> [output.txt]")
 		os.Exit(1)
 	}
 
-	data, err := os.ReadFile(os.Args[1])
+	inputFile := os.Args[1]
+	
+	// Determine output file: use second arg, or derive from input (.bin -> .txt)
+	var outputFile string
+	if len(os.Args) >= 3 {
+		outputFile = os.Args[2]
+	} else {
+		outputFile = strings.TrimSuffix(inputFile, ".bin") + ".txt"
+	}
+
+	data, err := os.ReadFile(inputFile)
 	if err != nil {
 		fmt.Printf("Error reading file: %v\n", err)
 		os.Exit(1)
 	}
 
+	// Create output file
+	f, err := os.Create(outputFile)
+	if err != nil {
+		fmt.Printf("Error creating output file: %v\n", err)
+		os.Exit(1)
+	}
+	defer f.Close()
+	output = f
+
 	policies := wls.GetRootAsPolicies(data, 0)
-	fmt.Printf("File: %s (%d bytes)\n", os.Args[1], len(data))
-	fmt.Printf("Number of policies: %d\n", policies.PoliciesLength())
-	fmt.Println(strings.Repeat("=", 60))
+	fmt.Fprintf(output, "File: %s (%d bytes)\n", inputFile, len(data))
+	fmt.Fprintf(output, "Number of policies: %d\n", policies.PoliciesLength())
+	fmt.Fprintln(output, strings.Repeat("=", 60))
 
 	for i := 0; i < policies.PoliciesLength(); i++ {
 		var policy wls.Policy
 		if policies.Policies(&policy, i) {
-			fmt.Printf("\nPolicy %d: %s\n", i, string(policy.Description()))
-			fmt.Println(strings.Repeat("-", 40))
+			fmt.Fprintf(output, "\nPolicy %d: %s\n", i, string(policy.Description()))
+			fmt.Fprintln(output, strings.Repeat("-", 40))
 
 			// Print actions
-			fmt.Printf("Actions (%d):\n", policy.ActionsLength())
+			fmt.Fprintf(output, "Actions (%d):\n", policy.ActionsLength())
 			for j := 0; j < policy.ActionsLength(); j++ {
 				var action wls.Action
 				if policy.Actions(&action, j) {
-					fmt.Printf("  [%d] %s: %s\n", j, action.Action().String(), string(action.Description()))
+					fmt.Fprintf(output, "  [%d] %s: %s\n", j, action.Action().String(), string(action.Description()))
 				}
 			}
 
 			// Print rules tree
-			fmt.Println("\nRules:")
+			fmt.Fprintln(output, "\nRules:")
 			if rules := policy.Rules(nil); rules != nil {
 				printNode(rules, "  ")
 			} else {
-				fmt.Println("  (none)")
+				fmt.Fprintln(output, "  (none)")
 			}
 		}
 	}
+
+	fmt.Printf("Output written to: %s\n", outputFile)
 }
 
