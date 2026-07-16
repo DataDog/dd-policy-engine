08f7079982a7b723a84736e17b8652df148aa7a0
anna.cai <anna.cai@datadoghq.com>
2026-07-16 10:14:02 -0400

    populate policy metadata before rule evaluation

--- c/src/evaluator.c
+++ c/src/evaluator.c
@@ -283,12 +283,6 @@ plcs_errors evaluate_policy(dd_ns(Policy_table_t) policy) {
   // extract actions
   dd_ns(Action_vec_t) actions = dd_ns(Policy_actions)(policy);
 
-  // extract rules
-  dd_ns(NodeTypeWrapper_table_t) rules = dd_ns(Policy_rules)(policy);
-
-  // // evaluate rules if they exist, otherwise return EVAL_RESULT_ABSTAIN
-  plcs_evaluation_result eval_res = rules ? evaluate_rules(rules, 0) : PLCS_EVAL_RESULT_ABSTAIN;
-
   // extract policy id, version, and description and set them in eval ctx
   dd_wls_UUID_struct_t policy_id = dd_ns(Policy_id)(policy);
   if (policy_id) {
@@ -299,6 +293,12 @@ plcs_errors evaluate_policy(dd_ns(Policy_table_t) policy) {
   }
   plcs_eval_ctx_set_policy_version(dd_ns(Policy_version)(policy));
   plcs_eval_ctx_set_policy_description(dd_ns(Policy_description)(policy));
+
+  // extract rules
+  dd_ns(NodeTypeWrapper_table_t) rules = dd_ns(Policy_rules)(policy);
+
+  // // evaluate rules if they exist, otherwise return EVAL_RESULT_ABSTAIN
+  plcs_evaluation_result eval_res = rules ? evaluate_rules(rules, 0) : PLCS_EVAL_RESULT_ABSTAIN;
 
   // perform actions given evaluation result
   return perform_actions(eval_res, actions);
